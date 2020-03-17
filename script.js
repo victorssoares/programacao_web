42c9b6820cb25a165604617da86d7dc3e33fe91d
victorssoares <60630624+victorssoares@users.noreply.github.com>
2020-03-17 20:57:23 -0300

    21 de boca atualizado!

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let divPai = document.getElementById('pai')
 let div = document.getElementById('teste')
+let gif = document.getElementById('gif')
 let jogo = document.getElementById('jogo')
 divPai.appendChild(div)
 let btn1 = document.getElementById('botao1')
@@ -13,10 +14,11 @@ let lista = document.getElementById('lista')
 let botao = document.createElement('button')
 botao.setAttribute('class', 'btn btn-primary')
 botao.innerText = 'Jogar!'
-botao.onclick = function () {
+botao.onclick = function() {
     jogar()
 }
 jogo.style.display = 'none'
+gif.style.display = 'none'
 
 function add() {
     if (nome1.value && nome2.value) {
@@ -37,14 +39,40 @@ function jogar() {
     jogo.style.display = 'block'
 }
 let soma1 = 0
+var contador = 0
 var jogadorAtual = nome2.value
+
 function somar(valor) {
+    contador++
+    let vencedor
+    if (contador % 2 == 0) {
+        vencedor = nome2.value
+
+    } else {
+        vencedor = nome1.value
+    }
+
+    function somar(valor) {
+        let soma = 0
+        if (soma < 21) {
+            let a = document.getElementById('valorAtual').innerHTML = soma = +valor
+        }
+    }
+
     soma1 += valor
+    switch (soma1) {
+        case 19:
+            btn3.setAttribute("disabled", 'true');
+            break;
+        case 20:
+            btn2.setAttribute("disabled", 'true')
+            btn3.setAttribute("disabled", 'true')
+    }
     if (soma1 < 21) {
-        if (jogadorAtual === nome2.value) {
-            jogadorAtual = nome1.value
-        } else {
+        if (jogadorAtual == nome1.value) {
             jogadorAtual = nome2.value
+        } else {
+            jogadorAtual = nome1.value
         }
         // alert(jogadorAtual)
         if (soma1 !== 1) {
@@ -53,28 +81,12 @@ function somar(valor) {
             return document.getElementById('valorAtual').innerHTML = `Jogador ${jogadorAtual} fez até o momento: ${soma1} ponto. Vez Proximo Jogador`
         }
     } else {
-        // alert("Jogo finalizado!")
-        if (jogadorAtual === 1) {
-            document.getElementById('valorAtual').innerHTML = `O vencedor é Jogador ${nome1.value}, com ${soma1} pontos`
-        } else {
-            if (soma1 > 21) {
-                alert('Seu burro! Perdeu')
-                document.getElementById('valorAtual').innerHTML = `O perdedor é Jogador ${nome1.value}, com ${soma1} pontos`
-            }else {
-                document.getElementById('valorAtual').innerHTML = `O Vencedor é Jogador ${nome2.value}, com ${soma1} pontos`
-            } 
-                btn1.setAttribute("disabled", 'true')
-                btn2.setAttribute("disabled", 'true')
-                btn3.setAttribute("disabled", 'true')
-        }
-        // btn1.setAttribute('disabled')
-
+        gif.style.display = 'block'
+        document.getElementById('valorAtual').innerHTML = `O vencedor é Jogador ${vencedor}, com ${soma1} pontos`
+        btn1.setAttribute("disabled", 'true')
+        btn2.setAttribute("disabled", 'true')
+        btn3.setAttribute("disabled", 'true')
     }
-}
+    btn1.setAttribute('disabled')
 
-// function somar(valor) {
-//     let soma = 0
-//     if (soma < 21) {
-//         let a = document.getElementById('valorAtual').innerHTML = soma = + valor
-//     }
-// }
+}
